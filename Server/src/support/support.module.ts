116393623441d624afab6ca3b301f0c5c6c4cdf8
tmothy khalayi <nandwatimothykhalayi@GMAIL.COM>
2025-09-26 11:48:05 +0300

    feat(support): enhance support ticket management with new endpoints and improved DTOs

--- Server/src/support/support.module.ts
+++ Server/src/support/support.module.ts
@@ -1,9 +1,21 @@
-import { Module } from '@nestjs/common';
+// src/modules/support/support.module.ts
+import { Module, forwardRef } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { SupportService } from './support.service';
 import { SupportController } from './support.controller';
+import { SupportTicket } from './entities/support-ticket.entity';
+import { SupportTicketResponse } from './entities/support-ticket-response.entity';
+import { UsersModule } from '../users/users.module';
+import { EmailModule } from '../../common/email/email.module';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([SupportTicket, SupportTicketResponse]),
+    forwardRef(() => UsersModule),
+    EmailModule,
+  ],
   controllers: [SupportController],
   providers: [SupportService],
+  exports: [SupportService, TypeOrmModule],
 })
 export class SupportModule {}
